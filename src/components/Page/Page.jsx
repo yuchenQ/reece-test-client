f1323e570033c02e91549a8762b81c24bd9c5f9a
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-03-04 23:26:57 +1100

    feat: page and file structure

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,45 @@
+/** @format */
+
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const StyledPage = styled.div`
+  box-sizing: border-box;
+  width: 1080px;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 1rem;
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-row-gap: 2rem;
+`;
+
+const Header = styled.section``;
+
+const Body = styled.section``;
+
+function Page({ className, header, children }) {
+  return (
+    <StyledPage>
+      <Layout className={className}>
+        <Header data-testid="header">{header}</Header>
+        <Body data-testid="body">{children}</Body>
+      </Layout>
+    </StyledPage>
+  );
+}
+
+Page.defaultProps = {
+  className: '',
+};
+
+Page.propTypes = {
+  className: PropTypes.string,
+  header: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Page;
